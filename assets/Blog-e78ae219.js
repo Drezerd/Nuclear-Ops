c2b708943e2829ebf4e3b921c4cdd86025cdc415
Drezerd <120797322+Drezerd@users.noreply.github.com>
2026-02-05 23:26:17 +0500

    Add files via upload

---
+++ assets/Blog-e78ae219.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-842d6f9a.js";const t={};function n(r,s){return c(),o("div",null," this is blog page ")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/Projects-24152748.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-842d6f9a.js";const r={};function s(t,n){return c(),o("div",null," This is projects page ")}const _=e(r,[["render",s]]);export{_ as default};
