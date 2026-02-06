3922e5965fc93f9c813e768dd5927ff0a7f26ffd
Drezerd <120797322+Drezerd@users.noreply.github.com>
2026-02-06 12:59:05 +0500

    Add files via upload

---
+++ assets/Service-6a063f7f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-36d16b35.js";const s={};function n(o,t){return c(),r("div",null," This is a service page ")}const _=e(s,[["render",n]]);export{_ as default};
